79b5ee6ee5f5cf5fee12a113b643c8ab79f69d9b
felipedr <felipedrios@hotmail.com>
2023-02-07 20:52:22 -0300

    Acerto do arquivo sql
    mudamos o banco de dados para rodar no postgreeSQL

--- db/dump.sql
+++ db/dump.sql
@@ -1,76 +1,85 @@
 create database pizzaria;
-use pizzaria;
 
 create table usuarios(
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	id SERIAL PRIMARY KEY,
 	nome VARCHAR(100) NOT NULL,
-	email VARCHAR(100) NOT NULL,
+	cpf VARCHAR(11) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
 	senha VARCHAR(100) NOT NULL,
 	telefone VARCHAR(20) NOT NULL,
-	tipo INTEGER NOT NULL,
+	tipo INTEGER NOT NULL
 );
 
 create table enderecos(
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	id SERIAL PRIMARY KEY,
 	rua VARCHAR(100) NOT NULL,
 	numero VARCHAR(10) NOT NULL,
 	complemento VARCHAR(50),
 	bairro VARCHAR(100) NOT NULL,
 	cidade VARCHAR(100) NOT NULL,
-	cep CHAR(9) NOT NULL,
+	cep VARCHAR(8) NOT NULL,
 	apelido VARCHAR(100),
 	usuario_id INTEGER NOT NULL,
 	CONSTRAINT fk_endereco_usuario FOREIGN KEY (usuario_id) REFERENCES usuarios (id)
 );
 
 create table ingredientes(
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	id SERIAL PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
 	adicional BOOLEAN NOT NULL,
-	valor DECIMAL NOT NULL
+	valor DECIMAL NOT NULL,
+	produto_ingrediente_id INTEGER NOT NULL,
+	ingredientes_adicionais_id INTEGER NOT NULL
 );
 
-create table sabores(
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+create table produtos_ingredientes(
+	id SERIAL PRIMARY KEY,
+	produto_id INTEGER NOT NULL,
+	ingrediente_id INTEGER NOT NULL
+);
+
+create table ingredientes_adicionais(
+	id SERIAL PRIMARY KEY,
+	pedido_produto_id INTEGER NOT NULL,
+	ingrediente_id INTEGER NOT NULL
+);
+
+create table produtos(
+	id SERIAL PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
-	ingrediente_id INTEGER NOT NULL,
-	CONSTRAINT fk_sabores_ingredientes FOREIGN KEY (IdIngredientesSabores) REFERENCES ingredientes (IdIngredientes)
+	preco DECIMAL NOT NULL,
+	categoria VARCHAR(30) NOT NULL,
+	tamanho VARCHAR(15) NOT NULL,
+	pedido_produto_id INTEGER NOT NULL,
+	produto_ingrediente_id INTEGER NOT NULL
 );
 
-create table produto(
-	IdProduto INTEGER PRIMARY KEY AUTO_INCREMENT,
-	Nome VARCHAR(30) NOT NULL,
-	Preco DECIMAL NOT NULL,
-	Categoria VARCHAR(30) NOT NULL,
-	Tamanho VARCHAR(15) NOT NULL,
-	IdSaboresProduto INTEGER NOT NULL,
-	CONSTRAINT fk_produto_sabores FOREIGN KEY (IdSaboresProduto) REFERENCES sabores (IdSabores)
+create table pedidos(
+	id SERIAL PRIMARY KEY,
+	valor_total DECIMAL NOT NULL,
+	observacao VARCHAR(50),
+	status_pedido INTEGER NOT NULL,
+	forma_de_pagamento INTEGER NOT NULL,
+	endereco_id INTEGER NOT NULL,
+	pedido_produto_id INTEGER NOT NULL
 );
 
-create table pedido(
-	IdPedido INTEGER PRIMARY KEY AUTO_INCREMENT,
-	ValorTotal DECIMAL NOT NULL,
-	Observacao VARCHAR(50) NULL,
-	StatusPedido Varchar(50) NOT NULL,
-	FormaDePagamento VARCHAR(30) NOT NULL,
-	IdUsuarioPedido INTEGER NOT NULL,
-	IdEnderecoPedido INTEGER NOT NULL,
-	IdProdutoPedido INTEGER NOT NULL,
-	IdSaboresPedido INTEGER NOT NULL,
-	IdIngredientesPedido INTEGER NOT NULL,
-	CONSTRAINT fk_pedido_usuario FOREIGN KEY (IdUsuarioPedido) REFERENCES usuario (IdUsuario),
-	CONSTRAINT fk_pedido_endereco FOREIGN KEY (IdEnderecoPedido) REFERENCES endereco (IdEndereco),
-	CONSTRAINT fk_pedido_produto FOREIGN KEY (IdProdutoPedido) REFERENCES produto (IdProduto),
-	CONSTRAINT fk_pedido_sabores FOREIGN KEY (IdSaboresPedido) REFERENCES sabores (IdSabores),
-	CONSTRAINT fk_pedido_ingredientes FOREIGN KEY (IdIngredientesPedido) REFERENCES ingredientes (IdIngredientes)
+create table pedidos_produtos(
+	id SERIAL PRIMARY KEY,
+	valor_unitario DECIMAL NOT NULL,
+	quantidade VARCHAR(2) NOT NULL,
+	produto_1_id INTEGER NOT NULL,
+	produto_2_id INTEGER NOT NULL,
+	pedido_id INTEGER NOT NULL
 );
 
-create table pedidoproduto(
-	IdPedidoProduto INTEGER PRIMARY KEY AUTO_INCREMENT,
-	ValorUnitario DECIMAL NOT NULL,
-	Quantidade VARCHAR(2) NOT NULL,
-	IdPedidoProdutoProduto INTEGER NOT NULL,
-	IdPedidoProdutoPedido INTEGER NOT NULL,
-	CONSTRAINT fk_pedidoproduto_produto FOREIGN KEY (IdPedidoProdutoProduto) REFERENCES produto (IdProduto),
-	CONSTRAINT fk_pedidoproduto_pedido FOREIGN KEY (IdPedidoProdutoPedido) REFERENCES pedido (IdPedido)
-);
+ALTER TABLE ingredientes ADD CONSTRAINT fk_ingredientes_adicionais FOREIGN KEY (ingredientes_adicionais_id) REFERENCES ingredientes_adicionais (id);
+ALTER TABLE pedidos ADD CONSTRAINT fk_pedido_endereco FOREIGN KEY (endereco_id) REFERENCES enderecos (id);
+ALTER TABLE produtos ADD CONSTRAINT fk_produto_pedido_produto FOREIGN KEY (pedido_produto_id) REFERENCES pedidos_produtos (id);
+ALTER TABLE pedidos_produtos ADD CONSTRAINT fk_pedido_produto_1 FOREIGN KEY (produto_1_id) REFERENCES produtos (id);
+ALTER TABLE pedidos_produtos ADD CONSTRAINT fk_pedido_produto_2 FOREIGN KEY (produto_2_id) REFERENCES produtos (id);
+ALTER TABLE pedidos_produtos ADD CONSTRAINT fk_pedido_pedido_produto FOREIGN KEY (pedido_id) REFERENCES pedidos (id);
+ALTER TABLE ingredientes_adicionais ADD CONSTRAINT fk_ingredientes_adicionais_pedido_produto FOREIGN KEY (pedido_produto_id) REFERENCES pedidos_produtos (id);
+ALTER TABLE ingredientes_adicionais ADD	CONSTRAINT fk_ingredientes_adicionais_ingrediente FOREIGN KEY (ingrediente_id) REFERENCES ingredientes (id);
+ALTER TABLE produtos_ingredientes ADD CONSTRAINT fk_produto_ingrediente_produto FOREIGN KEY (produto_id) REFERENCES produtos (id);
+ALTER TABLE produtos_ingredientes ADD CONSTRAINT fk_produto_ingrediente_ingrediente FOREIGN KEY (ingrediente_id) REFERENCES ingredientes (id);
